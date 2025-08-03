2437c7fb156bef651aeceeaf147679f11b11d554
Aftab Alam <aftabalamdlm@gmail.com>
2025-08-03 20:25:04 +0530

    fixed the error with upcoming meeting time

--- components/UpcomingMeetingTime.tsx
+++ components/UpcomingMeetingTime.tsx
@@ -7,7 +7,8 @@ import React from "react";
 const UpcomingMeetingTime = () => {
   const { getFirstUpcomingCallTime } = useGetCalls();
 
-  const time = getFirstUpcomingCallTime();
+  const timeData = getFirstUpcomingCallTime();
+  const time = timeData?.startsAt;
 
   return (
     <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal flex gap-2 items-center px-3 ">
